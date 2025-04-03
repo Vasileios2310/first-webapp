9c52c9ca300f3eade1d2e22b88042ce094405bfa
Siberian <vasileios.kritikos@aueb.gr>
2025-04-03 19:37:12 +0300

    teacher DTO

---
+++ WebAppDatabase/DTO/TeacherReadOnlyDTO.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppDatabase.DTO;
+
+public class TeacherReadOnlyDTO
+{
+   
+    public string? Firstname { get; set; }
+    
+    public string? Lastname { get; set; }
+    
+    public string? TaxNumber { get; set; }
+    
+    public string? Email { get; set; }
+
+    // public TeacherReadOnlyDTO() { }
+    //
+    // public TeacherReadOnlyDTO(string? firstname, string? lastname, string? taxNumber, string? email)
+    // {
+    //     Firstname = firstname;
+    //     Lastname = lastname;
+    //     TaxNumber = taxNumber;
+    //     Email = email;
+    // }
+}
---
+++ WebAppDatabase/DTO/TeacherUpdateDTO.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppDatabase.DTO;
+
+public class TeacherUpdateDTO
+{
+    [Required(ErrorMessage = "Firstname is required")]
+    [MinLength(3, ErrorMessage = "Firstname must be at least 3 characters long")]
+    public string? Firstname { get; set; }
+    
+    [Required(ErrorMessage = "Lastname is required")]
+    [MinLength(3, ErrorMessage = "Lastname must be at least 3 characters long")]
+    public string? Lastname { get; set; }
+    
+    [Required(ErrorMessage = "TaxNumber is required")]
+    [RegularExpression(@"^\d{9,10}$", ErrorMessage = "Invalid Tax Number. TaxNumber must be at least 9 characters long")]
+    public string? TaxNumber { get; set; }
+    
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string? Email { get; set; }
+
+    // public TeacherUpdateDTO() { }
+    //
+    // public TeacherUpdateDTO(string? firstname, string? lastname, string? taxNumber, string? email)
+    // {
+    //     Firstname = firstname;
+    //     Lastname = lastname;
+    //     TaxNumber = taxNumber;
+    //     Email = email;
+    // }
+}
